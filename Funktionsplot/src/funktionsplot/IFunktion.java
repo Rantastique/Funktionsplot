4276dc433d22b20f93d50998cf2ac0f72a0f6e13
Istvan Derda <istvan.gb.derda@gmail.com>
2018-07-11 16:14:56 +0200

    grundlegende Methoden für Funktionen provisorisch implementiert

--- Funktionsplot/src/funktionsplot/IFunktion.java
+++ Funktionsplot/src/funktionsplot/IFunktion.java
@@ -1,12 +1,32 @@
 package funktionsplot;
 
 
+import java.util.TreeMap;
+
 public interface IFunktion {
-	//Vorschläge
-	public double calcAt(Double x);
-	public double[] plot(double linkeIntervallgrenze, double rechteIntervallgrenze, int schritte); //Rückgabewert lieber eine hashmap?
-	//public double maximumIn(double[] plot);
-	//public double minimumIn(double[] plot);
+	
+	/*
+	 * Berechnet den Wert der Funktion an der Selle x
+	 * 
+	 * @param double x: x Wert
+	 * @return: f(x)
+	 */
+	public Double calcAt(double x);
+	
+	/*
+	 * Erstellt eine Hashmap mit diskreten Funktionswerten
+	 * 
+	 * @param double linkeIntervallgrenze: linke Intervallgrenze
+	 * @param double rechteIntervallgrenze: rechte Intervallgrenze
+	 * @param int schritte: Anzahl der Schritte im Intervall -> Anzahl zu berechnenden Stellen
+	 * 
+	 * @return: HashMap mit diskreten Funktionswerten
+	 */
+	public TreeMap<Double,Double> getValues(double linkeIntervallgrenze, double rechteIntervallgrenze, int schritte);
+	public Double maximumIn(TreeMap<Double, Double> values);
+	public Double minimumIn(TreeMap<Double, Double> values);
+	
+	//Vorschläge>:
 	//public double maximumIn(double linkeIntervallsgrenze, double rechteIntervallgrenze);
 	//public double minimumIn(double linkeIntervallsgrenze, double rechteIntervallgrenze);
 	//public Funktion ableitung();
