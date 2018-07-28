02fded2a09bec4558ecc608a8e15a4a627a69b13
Mariia Fortova <mariia.fortova@gmail.com>
2018-07-29 00:08:57 +0200

    Nullstellen_endlich_funktioniert

--- Funktionsplot/src/funktionsplot/Funktion.java
+++ Funktionsplot/src/funktionsplot/Funktion.java
@@ -71,6 +71,99 @@ public class Funktion implements IFunktion{
 	}
 
 	@Override
+	public TreeMap<Double, Double> nullstellen(double linkeIntervallgrenze, double rechteIntervallgrenze,int schritte) {
+		
+		double linksX = 0; //Intervall, wo sich die Nullstelle befindet
+		double rechtsX = 0;
+		
+		double abweichung = 0.0005;
+		
+		TreeMap<Double, Double> WT = berechneWertetabelle(linkeIntervallgrenze, rechteIntervallgrenze, schritte*2);
+		TreeMap<Double, Double> NST = new TreeMap<Double, Double>();
+		// Da die Abweichung nur für die Berührungspunkte passt, und nicht für die normale Schnittpunkte
+				// sollte es noch ein Verfahren geben, das zuerst den Vorzeichenwechsel findet und dann in diesem Bereich die Nullstelle
+				
+				double oldKey = WT.firstKey(); // für VZW-Suche
+				double oldValue = WT.get(WT.firstKey());
+						
+		
+		for (Map.Entry<Double, Double> entry: WT.entrySet()) {
+			System.out.println(entry.getValue());
+			if (Math.abs(entry.getValue()) < abweichung) {	// wenn der Punkt sich im bereich der NST befindet
+				System.out.println("gelandet!");
+				if(linksX == 0) {				// wenn der Punkt der erste im Intervall ist - > 
+					linksX = entry.getKey();	// - > linke Grenze setzen
+					System.out.println("linke Grenze gesetzt bei x = " + entry.getKey());
+				}
+				rechtsX = entry.getKey();		// immer die rechte Grenze setzen
+			}else{								// wenn der Punkt sich (noch/schon) nicht im Intervall befindet
+				                                
+											     	// wenn man vor kurzen aus dem Intervall weg ist
+				if(linksX != 0 && rechtsX != 0) {	// (NST im Intervall noch nicht gesucht)
+					
+					// such Minimum von abs im Intervall --- > Nullstelle
+					
+					Double minY = null;
+					Double minX = null;
+					TreeMap<Double, Double> values = berechneWertetabelle(linksX, rechtsX, schritte);
+					
+					for (Entry<Double, Double> entry2 : values.entrySet()) {
+						if((minY==null || Math.abs(entry2.getValue())<minY)) {
+							minY = Math.abs(entry2.getValue());
+							minX = entry2.getKey();
+						}
+					}
+					// Nullstelle zur Liste hinzufügen:
+					if( minX != values.firstKey())
+						NST.put((double) (Math.round(minX*100)/100.0), minY);
+					
+					// Intervall ist abgearbeitet:
+					linksX = 0;
+					rechtsX = 0;
+			    }
+			}
+			
+			// für normale Scnittpunkte (x^(ungeradeZahl) passen auch rein, theoretisch kann zur Doppelnullstellen führen)
+			
+			//letzte Bedingung : Abweichung um die Pollstellen auszuschliessen
+			if((oldValue<=0 && entry.getValue()>0 || oldValue>=0 && entry.getValue()<0) && Math.abs(oldValue) < 10000) { 
+				
+				// such Minimum von abs im Intervall --- > Nullstelle
+				
+				Double minYY = null;
+				Double minXX = null;
+				TreeMap<Double, Double> values = berechneWertetabelle(oldKey, entry.getKey(), schritte);
+				
+				for (Entry<Double, Double> entry2 : values.entrySet()) {
+					if( ( minYY==null || Math.abs(entry2.getValue())<minYY)) {
+						minYY = Math.abs(entry2.getValue());
+						minXX = entry2.getKey();
+					}
+				}
+				// Nullstelle zur Liste hinzufügen:
+				 if( minXX != values.firstKey())  // ohne Ränder (wegen Exponentialfunktionen)
+					 NST.put((double) (Math.round(minXX*100)/100.0), minYY);
+			}
+			
+			oldKey = entry.getKey();
+			oldValue = entry.getValue();
+		}
+		
+		//Zur Kontrolle:
+        System.out.println("Nullstellen:");
+		System.out.println();
+		
+		Iterator<Map.Entry<Double, Double> > it2 = NST.entrySet().iterator();
+        while(it2.hasNext()){
+            Map.Entry<Double, Double> en = it2.next();
+            System.out.println(  en.getKey() + "\" - \"" + en.getValue()  );
+        }
+        
+		return NST;
+	}
+
+	
+	/**
 	public TreeMap<Double,Double> nullstellen(double linkeIntervallgrenze, double rechteIntervallgrenze, int schritte) {
 		
 		TreeMap<Double, Double> WT = berechneWertetabelle(linkeIntervallgrenze, rechteIntervallgrenze, schritte*2);
@@ -136,7 +229,7 @@ public class Funktion implements IFunktion{
 				
 				System.out.println("ENDE DER INNEREN WERTETABELLE");
 									
-			} **/
+			} ////
 				if(altValue<0 && entry.getValue()>0 || altValue>0 && entry.getValue()<0)
 				{
 					System.out.println("Nullstellenbereich: " + altKey +": " + this.calcAt(altKey) + " - " +entry.getKey()+": " + this.calcAt(entry.getKey()));
@@ -174,7 +267,7 @@ public class Funktion implements IFunktion{
             System.out.println(  en.getKey() + "\" - \"" + en.getValue()  );
         }
 		return NST;
-	}
+	} **/
 
 	
 }
